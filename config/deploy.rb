19dc80e9ec8f0c203778afd6b0b2ad3a1121ef70
lululala <et3216@gmail.com>
2016-08-08 00:47:14 +0800

    done

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :application, 'fusakisql'
 set :repo_url, 'git@github.com:lustan3216/FUSAKIGG.git'
 set :deploy_to, '/home/deploy/rails-fusakisql'
 set :keep_releases, 5
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')  # 如果有 facebook.yml 或 email.yml 想要連結的話，也要加進來
+set :linked_files, fetch(:linked_files, []).push('config/pay2go.yml','config/facebook.yml','config/email.yml','config/database.yml', 'config/secrets.yml')  # 如果有 facebook.yml 或 email.yml 想要連結的話，也要加進來
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 set :passenger_restart_with_touch, true
 

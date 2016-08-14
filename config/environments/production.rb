e4abda8814769abd7ef58f748566f2c76e72c927
lululala <et3216@gmail.com>
2016-08-14 16:43:55 +0800

    parperclip setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,15 @@ Rails.application.configure do
   # be sure to have the adapter gem in your Gemfile and follow the adapter specific
   # installation and deployment instructions
   config.active_job.queue_adapter = :sidekiq
+  config.paperclip_defaults = {
+      :storage => :s3,
+      :s3_host_name => 's3-ap-northeast-1.amazonaws.com',
+      :path => '/:production',
+      :s3_credentials => {
+          :bucket => ENV['S3_BUCKET_NAME'],
+          :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+          :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      }
+  }
+
 end

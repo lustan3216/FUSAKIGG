7692cdd4dcd668f4d875ffe3d97f43db096993ac
lululala <et3216@gmail.com>
2016-10-08 01:25:29 +0800

    take out recomfirable

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,10 @@ class User < ApplicationRecord
   has_many :friends, through: :service_messages
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
-  devise :database_authenticatable, :registerable,:confirmable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
+
   serialize :fb_raw_data
   before_create :generate_authentication_token
 

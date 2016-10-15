668ea7e480fa00f4cf18646aa9366d243e5d40a1
lululala <et3216@gmail.com>
2016-10-15 11:02:34 +0800

    fix sitemap host

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://fusaki.com.tw"
+SitemapGenerator::Sitemap.default_host = "https://fusaki.com.tw"
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.

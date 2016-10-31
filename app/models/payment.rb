463434956c0c8d0c1432d5ff7eebbd656ed54da6
lululala <et3216@gmail.com>
2016-10-31 14:04:04 +0800

    fix order thank bug

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -22,7 +22,7 @@ class Payment < ApplicationRecord
 
   def self.find_and_process(params)
     result = JSON.parse( params['Result'] )
-    payment = self.find(result['MerchantOrderNo'].to_i)
+    payment = self.where(id:result['MerchantOrderNo'].to_i).last
     payment.paid = params['Status'] == 'SUCCESS'
     payment.params = params
     payment
